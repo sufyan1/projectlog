d3a7ac01b176e320b8d56dd1215d342b41206eb8
sufyan1 <sufyan_farooq656@yahoo.com>
2016-04-05 01:28:27 +0100

    user app modified Buttons added layout changet as well

--- Cricket User App/src/com/example/androidclient/BackgroundTask.java
+++ Cricket User App/src/com/example/androidclient/BackgroundTask.java
@@ -35,16 +35,16 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
     protected String doInBackground(String... params) {
      //   String reg_url = "http://192.168.0.7/webapp/register.php"; //home ip address
      //   String login_url = "http://192.168.0.7/webapp/login.php";//home ip address
-       String reg_url = "http://10.0.3.2/webapp/register.php";
-        String reg2_url = "http://10.0.3.2/webapp/reg2.php"; //for reg2 php
-           String login_url = "http://10.0.3.2/webapp/login.php";
-        String bat_url = "http://10.0.3.2/webapp/Current.php";
+     //  String reg_url = "http://10.0.3.2/webapp/register.php";
+        String Match_details = "http://10.0.3.2/webapp/Match_details.php"; //for reg2 php
+        String login_url = "http://10.0.3.2/webapp/login.php";
+        String At_Crease = "http://10.0.3.2/webapp/At_Crease.php";
         String method = params[0];
 
         if(method.equals("login"))
         {
             String id = params[1];
-          //  String Score = params[2];
+          //  String Match = params[2];
             try {
                 URL url = new URL(login_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
@@ -54,7 +54,7 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
                 String data = URLEncoder.encode("id","UTF-8")+"="+URLEncoder.encode(id,"UTF-8");
-                    //    URLEncoder.encode("Score","UTF-8")+"="+URLEncoder.encode(Score,"UTF-8");
+                //    URLEncoder.encode("Score","UTF-8")+"="+URLEncoder.encode(Score,"UTF-8");
                 bufferedWriter.write(data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
@@ -77,7 +77,78 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
                 e.printStackTrace();
             }
         }
-
+        else if(method.equals("Match"))
+        {
+            String id = params[1];
+           // String Match = params[1];
+            try {
+                URL url = new URL(Match_details);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String data = URLEncoder.encode("id","UTF-8")+"="+URLEncoder.encode(id,"UTF-8");
+                  //  URLEncoder.encode("id","UTF-8")+"="+URLEncoder.encode(id,"UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String response = "";
+                String line = "";
+                while ((line = bufferedReader.readLine())!=null)
+                {
+                    response+= line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return response;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else if(method.equals("Current"))
+        {
+            String id = params[1];
+            // String Match = params[1];
+            try {
+                URL url = new URL(At_Crease);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String data = URLEncoder.encode("id","UTF-8")+"="+URLEncoder.encode(id,"UTF-8");
+                //  URLEncoder.encode("id","UTF-8")+"="+URLEncoder.encode(id,"UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String response = "";
+                String line = "";
+                while ((line = bufferedReader.readLine())!=null)
+                {
+                    response+= line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return response;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return null;
     }
     @Override
--- Cricket User App/src/com/example/androidclient/MainActivity.java
+++ Cricket User App/src/com/example/androidclient/MainActivity.java
@@ -11,22 +11,36 @@ package com.example.androidclient;
 		import android.view.View;
 		import android.widget.EditText;
 public class MainActivity extends Activity{
-	EditText ID;
-	String id;
+	EditText ID,Match_Details;
+	String id,Match;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ID = (EditText)findViewById(R.id.id);
-	//	ET_PASS = (EditText)findViewById(R.id.user_pass);
+	//	Match_Details = (EditText)findViewById(R.id.m_d);
 	}
 
 
 	public void userLogin(View view)
 	{
 		id = ID.getText().toString();
-	//	Score = ET_PASS.getText().toString();
+		//Match = Match_Details.getText().toString();
 		String method = "login";
+		BackgroundTask backgroundTask = new BackgroundTask(this);
+		backgroundTask.execute(method,id);
+	}
+	public void matchLogin(View view)
+	{
+		id = ID.getText().toString();
+		String method = "Match";
+		BackgroundTask backgroundTask = new BackgroundTask(this);
+		backgroundTask.execute(method,id);
+	}
+	public void Current(View view)
+	{
+		id = ID.getText().toString();
+		String method = "Current";
 		BackgroundTask backgroundTask = new BackgroundTask(this);
 		backgroundTask.execute(method,id);
 	}
